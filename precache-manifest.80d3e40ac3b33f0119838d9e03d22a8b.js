6fa82cde37c6d946970ee6a24dc874c4c0f21ece
luke01455 <luke0145555@hotmail.co.uk>
2019-08-29 14:43:02 +0100

    Updates

---
+++ precache-manifest.80d3e40ac3b33f0119838d9e03d22a8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d255d3ff7485925409943d3ead1d0cac",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "87533dcd61cb93479e3d",
+    "url": "/crwn-clothing/static/css/main.9b6b3df5.chunk.css"
+  },
+  {
+    "revision": "673e7eb999e2d58f6d23",
+    "url": "/crwn-clothing/static/js/2.68ce2ebe.chunk.js"
+  },
+  {
+    "revision": "87533dcd61cb93479e3d",
+    "url": "/crwn-clothing/static/js/main.3e2810e3.chunk.js"
+  },
+  {
+    "revision": "e88d177d5d18f16bee32",
+    "url": "/crwn-clothing/static/js/runtime~main.675f06d2.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
