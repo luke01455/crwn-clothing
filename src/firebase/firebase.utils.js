da4cb874aac1c17f0346e13b11a9499384ceb817
luke01455 <luke0145555@hotmail.co.uk>
2019-09-01 11:52:41 +0100

    added displayname and email storing to firebase through google login

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,7 +15,22 @@ const config = {
   export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
 
-    console.log(firestore.doc('users/133dewfdsf'));
+    const userRef = firestore.doc(`user/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+   
+    if(!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({ displayName, email, createdAt, ...additionalData})
+      } catch (error ) {
+        console.log('error creating user', error.message)
+      }
+    }
+
+    return userRef;
   }
 
   firebase.initializeApp(config);
