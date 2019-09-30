cb0d4b8121a00238548292a6d50c79c4bf8b0b79
luke01455 <luke0145555@hotmail.co.uk>
2019-09-30 17:03:28 +0100

    checkout-item xferred to css in js

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -3,26 +3,26 @@ import { connect } from 'react-redux';
 
 import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
 
-import './checkout-item.styles.scss';
+import { CheckoutItemContainer, ImageContainer, Name, Quantity, Price, RemoveButton, Value, Arrow } from './checkout-item.styles'
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
     const { name, imageUrl, price, quantity} = cartItem
     return (
-    <div className='checkout-item'>
-        <div className='image-container'>
+    <CheckoutItemContainer>
+        <ImageContainer>
             <img src={imageUrl} alt='item'/>
-        </div>
-        <span className='name'>{name}</span>
-        <span className='quantity'>
-        <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
-        <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
-        </span>
-        <span className='price'>${price}</span>
-        <div className='remove-button' onClick={() => clearItem(cartItem)}>
+        </ImageContainer>
+        <Name>{name}</Name>
+        <Quantity>
+        <Arrow onClick={() => removeItem(cartItem)}>&#10094;</Arrow>
+        <Value>{quantity}</Value>
+        <Arrow onClick={() => addItem(cartItem)}>&#10095;</Arrow>
+        </Quantity>
+        <Price>${price}</Price>
+        <RemoveButton onClick={() => clearItem(cartItem)}>
         &#10005;
-      </div>
-    </div>
+      </RemoveButton>
+    </CheckoutItemContainer>
 )};
 
 const mapDispatchToProps = dispatch => ({
