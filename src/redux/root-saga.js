586e9b5af5b9ce2e3860fea3a5b8a3e3417db003
luke01455 <luke0145555@hotmail.co.uk>
2019-10-05 19:23:08 +0100

    creating the root saga file

---
+++ src/redux/root-saga.js
@@ -0,0 +1,8 @@
+import { all, call } from 'redux-sage/effects';
+import { fetchCollectionsStart } from './shop/shop.sagas'
+
+export default function* rootSaga() {
+    yield all([
+        call(fetchCollectionStart)
+    ]);
+}
