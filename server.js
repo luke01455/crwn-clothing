278f977358120b7429a8dbe9ad5d27725ab686e4
luke01455 <luke0145555@hotmail.co.uk>
2019-10-11 21:15:15 +0100

    connected the client to server and got stripe test payments working, also spent a while fixing a bug where i'd put .env file in wrong folder

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -15,31 +16,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-    app.get('*', function(req, res){
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-    });
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 app.listen(port, error => {
-    if(error) throw error;
-    console.log('Server running on port ' + port)
+  if (error) throw error;
+  console.log('Server running on port ' + port);
 });
 
 app.post('/payment', (req, res) => {
-    const body = {
-        source: req.body.token.id,
-        amount: req.body.amount,
-        currency: 'usd'
-    }
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  };
 
-    stripe.charges.create(body, (stripeErr, stripeRes) => {
-        if (stripeErr) {
-            res.status(500).send({ error: stripeErr })
-        } else {
-            res.status(200).send({ success: stripeRes })
-        }
-    })
-})
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
+});
