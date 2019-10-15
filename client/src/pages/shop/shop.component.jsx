b3d9dbb38f543eeb1f2cea6baff9551aae222eeb
luke01455 <luke0145555@hotmail.co.uk>
2019-10-15 20:49:38 +0100

    put lazy loading on shop page

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
-import CollectionPageContainer from '../collection/collection.container';
+import Spinner from '../../components/spinner/spinner.component';
 
+const CollectionsOverviewContainer = lazy(() => import('../../components/collections-overview/collections-overview.container'));
+const CollectionPageContainer = lazy(() => import('../collection/collection.container'));
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
 
@@ -17,6 +18,7 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
 
         return (
         <div className='shop-page'>
+        <Suspense fallback={<Spinner />}>
         <Route 
         exact path ={`${match.path}`} 
         component={CollectionsOverviewContainer} />
@@ -24,6 +26,7 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
         path={`${match.path}/:collectionId`} 
         component={CollectionPageContainer}
         />
+        </Suspense>
     </div>)
 }
 
