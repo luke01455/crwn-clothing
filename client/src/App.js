7cc3c4ff81d4b23764ef44ce1c6a9123ac8a8d2a
luke01455 <luke0145555@hotmail.co.uk>
2019-10-15 20:03:43 +0100

    made homepage and routes off it lazy load .

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,18 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
+import Spinner from './components/spinner/spinner.component';
 
 import { GlobalStyle } from './global.styles'
-
-
 
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
-const HomePage = lazy(() => import('./pages/homepage/homepage.component'))
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
 
 const App = ({ checkUserSession, currentUser }) => {
     //persisting user
@@ -29,9 +27,9 @@ const App = ({ checkUserSession, currentUser }) => {
         <GlobalStyle />
           <Header />
           <Switch>
-            <Suspense fallback={<div> Loading...</div>}>
+            <Suspense fallback={<Spinner />}>
             <Route exact path="/" component={HomePage} />
-            </Suspense>
+            
             <Route path="/shop" component={ShopPage} />
             <Route exact path="/checkout" component={CheckoutPage}/>
             <Route
@@ -45,6 +43,7 @@ const App = ({ checkUserSession, currentUser }) => {
                 )
               }
             />
+            </Suspense>
           </Switch>
       </div>
     );
